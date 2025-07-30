bd7239e8e24fb708045f0e52df88fbca170bebf1
muiruritrevor <trevormuirurii@gmail.com>
2025-07-30 23:22:22 +0300

    Initial Commit

--- python-context-async-perations-0x02/1-execute.py
+++ python-context-async-perations-0x02/1-execute.py
@@ -1,4 +1,12 @@
+import asnycio
 import sqlite3
 
-class ExecuteQuery:
-    pass
+asycio def execute_query(self, query):
+        """Asynchronously execute a query on the database."""
+        async with DatabaseConnection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(query)
+            conn.commit()
+            return cursor.fetchall()
+        # Return False to propagate exceptions, True to suppress them
+        return False
