30db056bcf9ecf2996d3a0442ba73a052611cedc
muiruritrevor <trevormuirurii@gmail.com>
2025-08-03 00:56:34 +0300

    Add reusable context manager for executing SQL queries

--- python-context-async-perations-0x02/1-execute.py
+++ python-context-async-perations-0x02/1-execute.py
@@ -1,23 +1,29 @@
 import sqlite3
 
-# class ExecuteQuery(query):
-#     def __init__(self):
-#         self.connection = None
-#         self.query = query
-    
-#     def __enter__(self):
-#         self.connection = sqlite3.connect('users.db')
-#         cursor = self.connection.cursor()
-#         cursor.execute(self.query)
-#         self.connection.commit()
-#         print(cursor.fetchall())
-#         return self.connection
-    
-#     def __exit__(self, exc_type, exc_value, traceback):
-#         if self.connection:
-#             self.connection.close()
-#         # Return False to propagate exceptions, True to suppress them
-#         return False
-
 class ExecuteQuery:
-    pass
+    """A reusable context manager to execute SQL queries and manage database connections."""
+    def __init__(self, query, params=None):
+        self.query = query
+        self.params = params if params is not None else ()
+        self.connection = None
+        self.result = None
+
+    def __enter__(self):
+        self.connection = sqlite3.connect('users.db')
+        cursor = self.connection.cursor()
+        cursor.execute(self.query, self.params)
+        self.result = cursor.fetchall()
+        return self.result
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.connection:
+            self.connection.close()
+        return False  # Propagate any exception if it occurs
+
+
+query = "SELECT * FROM users WHERE age > ?"
+params = (25,)
+
+with ExecuteQuery(query, params) as result:
+    for row in result:
+        print(row)
