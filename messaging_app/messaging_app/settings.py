398c2f0e562ce1b34f67bee92bae400806314662
muiruritrevor <trevormuirurii@gmail.com>
2025-09-17 14:07:04 +0300

    Update installed_apps

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "chats.apps.ChatsConfig",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
