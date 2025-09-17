55c5f7ec2cdae5ec020dc7cd49457d6801dc8f1f
muiruritrevor <trevormuirurii@gmail.com>
2025-09-17 14:29:01 +0300

    Add User, Message and Coversation models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    role = models.CharField(max_length=20, choices=[('guest', 'Guest'),('host', 'Host'), ('admin', 'Admin')], default='user')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender.username} to {self.receiver.username} at {self.timestamp}"
+    
+class Conversation(models.Model):
+    participants = models.ManyToManyField(User, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.id} with {self.participants.count()} participants"
+    
