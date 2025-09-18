c9ffb1087c1d1c4c8629b0b0534d0124326d61f2
muiruritrevor <trevormuirurii@gmail.com>
2025-09-18 10:09:28 +0300

    update User

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,34 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 from datetime import datetime
-
+import uuid
 # Create your models here.
 
 
-class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
-    role = models.CharField(max_length=20, choices=[('guest', 'Guest'),('host', 'Host'), ('admin', 'Admin')], default='user')
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=20, blank=False)
+    last_name = models.CharField(max_length=20, blank=False)
+    email = models.EmailField(unique=True, blank=False)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    ROLE_CHOICES = ['guest', 'host', 'admin']
+    role = models.CharField(max_length=20, choices=[(role, role.capitalize()) for role in ROLE_CHOICES], default='guest')
     created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.first_name + ' ' + self.last_name
-
-
-class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Message from {self.sender.username} to {self.receiver.username} at {self.timestamp}"
     
+    def __str__(self):
+        return f"{self.first_name} + ' ' + {self.last_name} ({self.role})"  
+    
+
 class Conversation(models.Model):
     participants = models.ManyToManyField(User, related_name='conversations')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Conversation {self.id} with {self.participants.count()} participants"
+            participant_names = ', '.join([p.username for p in self.participants.all()])
+            return f"Conversation {self.id} with {participant_names}"
+    
+    
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender.username} to {self.receiver.username} at {self.timestamp}"
     
