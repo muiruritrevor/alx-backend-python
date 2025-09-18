88c115d096326945c1d92f33812820cb93282ce3
muiruritrevor <trevormuirurii@gmail.com>
2025-09-18 10:39:45 +0300

    set up url routing

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
 
 # Create your views here.
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
+
+    # def get_queryset(self):
+    #     user = self.request.user
+    #     return Message.objects.filter(conversation__participants=user)
+    
+
+
+
+
