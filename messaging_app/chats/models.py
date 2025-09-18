490e2f15f878f3cad537bfc0301a0a042b6d6971
muiruritrevor <trevormuirurii@gmail.com>
2025-09-18 10:13:23 +0300

    Fix to align with checker

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -12,6 +12,7 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=20, blank=False)
     email = models.EmailField(unique=True, blank=False)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
+    password = models.CharField(max_length=128, blank=False)
     ROLE_CHOICES = ['guest', 'host', 'admin']
     role = models.CharField(max_length=20, choices=[(role, role.capitalize()) for role in ROLE_CHOICES], default='guest')
     created_at = models.DateTimeField(auto_now_add=True)
@@ -31,10 +32,11 @@ class Conversation(models.Model):
     
     
 class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Message from {self.sender.username} to {self.receiver.username} at {self.timestamp}"
